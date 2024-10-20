edaff8e22864fa04f5d62d60a7dbae138b36bf8a
wltnryu <yebbrnjs@gmail.com>
2024-10-20 20:15:36 +0900

    [#28] FigureCell 크기 설정 및 스크롤 및 확대축소 방지

--- Project/Reazy/Views/PDF/Menu/FigureCell.swift
+++ Project/Reazy/Views/PDF/Menu/FigureCell.swift
@@ -26,6 +26,7 @@ struct PDFKitView: UIViewRepresentable {
     pdfView.displayMode = .singlePageContinuous
     pdfView.pageShadowsEnabled = false
     pdfView.subviews.first!.backgroundColor = .white
+    pdfView.isUserInteractionEnabled = false
     
     return pdfView
   }
@@ -44,38 +45,44 @@ struct FigureCell: View {
   let index: Int
   @State private var isDragging = false
   
+  @State private var aspectRatio: CGFloat = 1.0
+  
   var body: some View {
     VStack(spacing: 0) {
       ZStack  {
         if let document = originalViewModel.setFigureDocument(for: index) {
-          PDFKitView(document: document)
-            .edgesIgnoringSafeArea(.all)        // 전체 화면에 맞추기
-            .frame(minHeight: 152)
-            .padding(8)
-            .onDrag {
-              if let data = document.dataRepresentation() {
-                isDragging = true
-                let head = originalViewModel.figureAnnotations[index].head
-                let itemProvider = NSItemProvider(item: data as NSData, typeIdentifier: UTType.pdf.identifier)
-                itemProvider.suggestedName = head
-                return itemProvider
+          if let page = document.page(at: 0) {
+            let pageRect = page.bounds(for: .mediaBox)
+            let aspectRatio = pageRect.width / pageRect.height
+            
+            PDFKitView(document: document)
+              .edgesIgnoringSafeArea(.all)        // 전체 화면에 맞추기
+              .frame(width: 200, height: 200 / aspectRatio)
+              .padding(8)
+              .onDrag {
+                if let data = document.dataRepresentation() {
+                  isDragging = true
+                  let head = originalViewModel.figureAnnotations[index].head
+                  let itemProvider = NSItemProvider(item: data as NSData, typeIdentifier: UTType.pdf.identifier)
+                  itemProvider.suggestedName = head
+                  return itemProvider
+                }
+                return NSItemProvider()
+              } preview: {
+                if isDragging {
+                  PDFKitView(document: document)
+                    .frame(width: 0, height: 0)
+                } else {
+                  Color.clear.frame(width: 0, height: 0)
+                }
               }
-              return NSItemProvider()
-            } preview: {
-              // TODO: - 브리 필요한지 확인
-              if isDragging {
-                PDFKitView(document: document)
-                  .frame(width: 0, height: 0)
-              } else {
-                Color.clear.frame(width: 0, height: 0)
+              .onDisappear {
+                isDragging = false
               }
-            }
-            .onDisappear {
-              isDragging = false
-            }
-          
-          RoundedRectangle(cornerRadius: 8)
-            .stroke(Color(hex: "#CDCFE1"), lineWidth: 1)
+            
+            RoundedRectangle(cornerRadius: 8)
+              .stroke(Color(hex: "#CDCFE1"), lineWidth: 1)
+          }
         } else {
           Text("pdf 로드 실패 ")
         }
