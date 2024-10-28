e78be6c6c5ca2f08de6c9b8d19ee6e18f1d4b730
Dorii0513 <bobodorii0513@gmail.com>
2024-10-28 17:09:43 +0900

    [#58] CommentView 그리기

--- Project/Reazy/Views/PDF/CommentView.swift
+++ Project/Reazy/Views/PDF/CommentView.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct CommentView: View {
+    
+    @StateObject var commentViewModel: CommentViewModel
+    @State var text: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField(
+                LocalizedStringKey("AddComment"), text: $text,
+                prompt: Text("코멘트 추가").foregroundColor(.primary4)
+            )
+            .foregroundStyle(.point2)
+            .padding(.vertical, 14)
+            .padding(.horizontal, 14)
+            
+            HStack {
+                Spacer()
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "arrow.up.circle.fill")
+                        .foregroundStyle(text.isEmpty ? .primary4 : .point2)
+                        .font(.system(size: 20))
+                })
+            }
+                .padding(.top, 14)
+                .padding(.bottom, 8)
+                .padding(.trailing, 12)
+        }
+        .background(Color.gray200)
+        .cornerRadius(12)
+        .frame(width: 312)
     }
 }
 
+
+
 #Preview {
-    CommentView()
+    CommentView(commentViewModel: CommentViewModel(comments: []))
 }
