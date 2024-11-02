3a014162350fe2e95a33245d1cf07738b96269f4
minjung0067 <minjung0067@naver.com>
2024-11-02 15:36:19 +0900

    [#87] 앱 전체 tint color 변경

--- Project/Reazy/App/AppView.swift
+++ Project/Reazy/App/AppView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct AppView: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // 전체 Tint Color 설정
+        UIView.appearance().tintColor = UIColor.primary1
+
+        return true
     }
 }
