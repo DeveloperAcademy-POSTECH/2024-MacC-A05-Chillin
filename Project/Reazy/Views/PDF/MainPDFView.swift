cd6897613d5c9cab6cc2e10b64fffaab937c8246
wltnryu <yebbrnjs@gmail.com>
2024-11-03 13:05:15 +0900

    [#96] SplitView 위치 변경 시 toolMode 초기화되는 문제 해결

--- Project/Reazy/Views/PDF/MainPDFView.swift
+++ Project/Reazy/Views/PDF/MainPDFView.swift
@@ -101,7 +101,7 @@ struct MainPDFView: View {
                         if selectedMode == "원문 모드" {
                             HStack(spacing: 0) {
                                 Spacer()
-                            
+                                
                                 ForEach(WriteButton.allCases, id: \.self) { btn in
                                     // 조건부 Padding값 조정
                                     let trailingPadding: CGFloat = {
@@ -113,7 +113,7 @@ struct MainPDFView: View {
                                             return 32
                                         }
                                     }()
-                                
+                                    
                                     // [Comment], [Highlight], [Pencil], [Eraser], [Translate] 버튼
                                     WriteViewButton(button: $selectedButton, HighlightColors: $selectedColor, buttonOwner: btn) {
                                         // MARK: - 작성 관련 버튼 action 입력
@@ -124,37 +124,37 @@ struct MainPDFView: View {
                                         } else {
                                             selectedButton = btn
                                         }
-                                    
+                                        
                                         switch selectedButton {
                                         case .translate:
                                             NotificationCenter.default.post(name: .PDFViewSelectionChanged, object: nil)
                                             mainPDFViewModel.toolMode = .translate
-                                        
+                                            
                                         case .pencil:
                                             mainPDFViewModel.toolMode = .pencil
-                                        
+                                            
                                         case .eraser:
                                             mainPDFViewModel.toolMode = .eraser
-                                        
+                                            
                                         case .highlight:
                                             mainPDFViewModel.toolMode = .highlight
-                                        
+                                            
                                         case .comment:
                                             mainPDFViewModel.toolMode = .comment
-                                        
+                                            
                                         default:
                                             // 전체 비활성화
                                             mainPDFViewModel.toolMode = .none
                                         }
                                     }
                                     .padding(.trailing, trailingPadding)
-                                
+                                    
                                     // Highlight 버튼이 선택될 경우 색상을 선택
                                     if selectedButton == .highlight && btn == .highlight {
                                         highlightColorSelector()
                                     }
                                 }
-                            
+                                
                                 Spacer()
                             }
                             .background(.clear)
@@ -341,18 +341,23 @@ struct MainPDFView: View {
                 .environmentObject(mainPDFViewModel)
         }
     }
-  
+    
     var verticalLayout: some View {
         VStack(spacing: 0) {
             if isPaperViewFirst {
                 mainView(for: selectedMode)
+                    .onAppear {
+                        if let selectedButton = selectedButton {
+                            updateToolMode(for: selectedButton)
+                        }
+                    }
             }
-      
+            
             if floatingViewModel.splitMode, let splitDetails = floatingViewModel.getSplitDocumentDetails() {
                 Rectangle()
                     .frame(height: 1)
                     .foregroundStyle(isPaperViewFirst ? .gray300 : .clear)
-        
+                
                 FloatingSplitView(
                     documentID: splitDetails.documentID,
                     document: splitDetails.document,
@@ -366,29 +371,39 @@ struct MainPDFView: View {
                 )
                 .environmentObject(mainPDFViewModel)
                 .environmentObject(floatingViewModel)
-        
+                
                 Rectangle()
                     .frame(height: 1)
                     .foregroundStyle(isPaperViewFirst ? .clear : .gray300)
-                }
-      
+            }
+            
             if !isPaperViewFirst {
                 mainView(for: selectedMode)
+                    .onAppear {
+                        if let selectedButton = selectedButton {
+                            updateToolMode(for: selectedButton)
+                        }
+                    }
             }
         }
     }
-  
+    
     var horizontalLayout: some View {
         HStack(spacing: 0) {
             if isPaperViewFirst {
                 mainView(for: selectedMode)
+                    .onAppear {
+                        if let selectedButton = selectedButton {
+                            updateToolMode(for: selectedButton)
+                        }
+                    }
             }
-      
+            
             if floatingViewModel.splitMode, let splitDetails = floatingViewModel.getSplitDocumentDetails() {
                 Rectangle()
                     .frame(width: 1)
                     .foregroundStyle(isPaperViewFirst ? .gray300 : .clear)
-        
+                
                 FloatingSplitView(
                     documentID: splitDetails.documentID,
                     document: splitDetails.document,
@@ -402,23 +417,28 @@ struct MainPDFView: View {
                 )
                 .environmentObject(mainPDFViewModel)
                 .environmentObject(floatingViewModel)
-        
+                
                 Rectangle()
                     .frame(width: 1)
                     .foregroundStyle(isPaperViewFirst ? .clear : .gray300)
             }
-      
+            
             if !isPaperViewFirst {
                 mainView(for: selectedMode)
+                    .onAppear {
+                        if let selectedButton = selectedButton {
+                            updateToolMode(for: selectedButton)
+                        }
+                    }
             }
         }
     }
-  
+    
     // 기기의 방향에 따라 isVertical 상태를 업데이트하는 함수
     private func updateOrientation(with geometry: GeometryProxy) {
         isVertical = geometry.size.height > geometry.size.width
     }
-
+    
     @ViewBuilder
     private func highlightColorSelector() -> some View {
         Rectangle()
@@ -435,12 +455,27 @@ struct MainPDFView: View {
             }
             .padding(.trailing, color == .blue ? .zero : 18)
         }
- 
+        
         Rectangle()
             .frame(width: 1, height: 19)
             .foregroundStyle(.primary4)
             .padding(.leading, 24)
             .padding(.trailing, 17)
+    }
+    
+    private func updateToolMode(for button: WriteButton) {
+        switch button {
+        case .translate:
+            mainPDFViewModel.toolMode = .translate
+        case .pencil:
+            mainPDFViewModel.toolMode = .pencil
+        case .eraser:
+            mainPDFViewModel.toolMode = .eraser
+        case .highlight:
+            mainPDFViewModel.toolMode = .highlight
+        case .comment:
+            mainPDFViewModel.toolMode = .comment
+        }
     }
 }
 
