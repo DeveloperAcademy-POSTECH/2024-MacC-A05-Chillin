ba3723503a35dbdc9776b8365719ec4355edf397
mooninbeom <dlsqja567@naver.com>
2024-11-04 00:36:39 +0900

    [#94] 네트워크 요청 메소드 생성

--- Project/Reazy/Domain/Network/NetworkManager + Server.swift
+++ Project/Reazy/Domain/Network/NetworkManager + Server.swift
@@ -9,7 +9,45 @@ import Foundation
 
 
 extension NetworkManager {
-    static func fetchPDFLayoutData() throws -> PDFInfo {
-        throw NetworkManagerError.invalidURL
+    static func fetchPDFLayoutData(pdfURL: URL) async throws -> PDFInfo {
+        guard let urlString = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String else {
+            throw NetworkManagerError.invalidInfo
+        }
+        
+        guard let url = URL(string: urlString) else {
+            throw NetworkManagerError.invalidURL
+        }
+        
+        guard let pdfData = try? Data(contentsOf: pdfURL) else {
+            throw NetworkManagerError.invalidPDF
+        }
+        
+        let boundary = "Boundary-\(UUID().uuidString)"
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        
+        var body = Data()
+        let fileName = pdfURL.lastPathComponent
+        body.append("--\(boundary)\r\n".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name\"file\"; filename=\"\(fileName)\"\r\n".data(using: .utf8)!)
+        body.append("Content-Type: application/pdf".data(using: .utf8)!)
+        body.append(pdfData)
+        body.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+        
+        request.httpBody = body
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        if let response = response as? HTTPURLResponse,
+           !(200..<300 ~= response.statusCode) {
+            throw NetworkManagerError.badRequest
+        }
+        
+        let decoder = JSONDecoder()
+        let decodedData = try decoder.decode(PDFInfo.self, from: data)
+        return decodedData
     }
 }
