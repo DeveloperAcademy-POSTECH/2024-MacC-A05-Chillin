81519fd18fa53a9298bcd0b33ec53185b82d016e
mooninbeom <dlsqja567@naver.com>
2024-11-03 23:59:19 +0900

    [#94] config 파일 추가, gitignore 수정

---
+++ Project/Reazy/Domain/Network/NetworkManager + Server.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager + Server.swift
+//  Reazy
+//
+//  Created by 문인범 on 11/3/24.
+//
+
+import Foundation
+
+
+extension NetworkManager {
+    static func fetchPDFLayoutData() throws -> PDFInfo {
+        
+    }
+}
