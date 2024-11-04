11d661c8be25410ea999cc86a88e8f6ad500a1c7
wltnryu <yebbrnjs@gmail.com>
2024-11-04 21:35:26 +0900

    [#109] PaperListCell icon 파일 변경(버전 17.4 포함)

--- Project/Reazy/Views/Home/Paper/PaperListCell.swift
+++ Project/Reazy/Views/Home/Paper/PaperListCell.swift
@@ -8,85 +8,86 @@
 import SwiftUI
 
 struct PaperListCell: View {
-  
-  let title: String
-  let date: String
-  let isSelected: Bool
-  let isEditing: Bool
-  let isEditingSelected: Bool
-  let onSelect: () -> Void
-  let onEditingSelect: () -> Void
-  
-  var body: some View {
-    ZStack {
-      if isSelected && !isEditing {
-        RoundedRectangle(cornerRadius: 14)
-          .fill(.primary2)
-          .padding(.vertical, 3)
-          .padding(.horizontal, 8)
-      }
-      
-      HStack(spacing: 0) {
-        if isEditing {
-          Image(systemName: isEditingSelected ? "checkmark.circle.fill" : "circle")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 24, height: 24)
-            .foregroundStyle(isEditingSelected ? .primary1 : .primary4)
-            .onTapGesture {
-              onEditingSelect()
+    
+    let title: String
+    let date: String
+    let isSelected: Bool
+    let isEditing: Bool
+    let isEditingSelected: Bool
+    let onSelect: () -> Void
+    let onEditingSelect: () -> Void
+    
+    var body: some View {
+        ZStack {
+            if isSelected && !isEditing {
+                RoundedRectangle(cornerRadius: 14)
+                    .fill(.primary2)
+                    .padding(.vertical, 3)
+                    .padding(.horizontal, 8)
             }
-            .padding(.trailing, 20)
+            
+            HStack(spacing: 0) {
+                if isEditing {
+                    Image(systemName: isEditingSelected ? "checkmark.circle.fill" : "circle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(isEditingSelected ? .primary1 : .primary4)
+                        .onTapGesture {
+                            onEditingSelect()
+                        }
+                        .padding(.trailing, 20)
+                }
+                
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 42, height: 42)
+                    .foregroundStyle(.gray500)
+                    .overlay(
+                        Image("document")
+                            .renderingMode(.template)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 21)
+                            .foregroundStyle(.gray100)
+                    )
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(title)
+                        .lineLimit(2)
+                        .reazyFont(.h2)
+                        .foregroundStyle(.gray900)
+                        .padding(.bottom, 6)
+                    Text(date)
+                        .reazyFont(.h4)
+                        .foregroundStyle(.gray600)
+                }
+                .padding(.leading, 15)
+                
+                Spacer()
+            }
+            .background(.clear)
+            .padding(.horizontal, 22)
+            .padding(.vertical, 15)
+            .contentShape(Rectangle())
+            .onTapGesture {
+                if isEditing {
+                    onEditingSelect()
+                } else {
+                    onSelect()
+                }
+            }
         }
-        
-        RoundedRectangle(cornerRadius: 10)
-          .frame(width: 42, height: 42)
-          .foregroundStyle(.gray500)
-          .overlay(
-            Image(systemName: "text.page.fill")
-              .resizable()
-              .scaledToFit()
-              .frame(height: 21)
-              .foregroundStyle(.gray100)
-          )
-        
-        VStack(alignment: .leading, spacing: 0) {
-          Text(title)
-            .lineLimit(2)
-            .reazyFont(.h2)
-            .foregroundStyle(.gray900)
-            .padding(.bottom, 6)
-          Text(date)
-            .reazyFont(.h4)
-            .foregroundStyle(.gray600)
-        }
-        .padding(.leading, 15)
-        
-        Spacer()
-      }
-      .background(.clear)
-      .padding(.horizontal, 22)
-      .padding(.vertical, 15)
-      .contentShape(Rectangle())
-      .onTapGesture {
-        if isEditing {
-          onEditingSelect()
-        } else {
-          onSelect()
-        }
-      }
     }
-  }
 }
 
 #Preview {
-  PaperListCell(
-    title: "test",
-    date: "1시간 전",
-    isSelected: false,
-    isEditing: true,
-    isEditingSelected: false,
-    onSelect: {},
-    onEditingSelect: {}
-  )
+    PaperListCell(
+        title: "test",
+        date: "1시간 전",
+        isSelected: false,
+        isEditing: true,
+        isEditingSelected: false,
+        onSelect: {},
+        onEditingSelect: {}
+    )
 }
