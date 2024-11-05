530ef6879da9d499812fe9da5aa38aabb43f7234
mooninbeom <dlsqja567@naver.com>
2024-11-05 23:10:07 +0900

    [#120] 퍼미션 수정

--- Project/Reazy/Domain/Helpers/PDFFileManager.swift
+++ Project/Reazy/Domain/Helpers/PDFFileManager.swift
@@ -30,46 +30,53 @@ extension PDFFileManager {
         
         guard let url = url.first else { return }
         
-        let result: PDFInfo = try await NetworkManager.fetchPDFExtraction(process: .processHeaderDocument, pdfURL: url)
-        
-        let names = result.names?.reduce("") { $0 + $1 + "," } ?? "알 수 없음"
-        
-        let tempDoc = PDFDocument(url: url)
-        
-        let pageCount = tempDoc?.pageCount
-        
-        //TODO: 데이터 저장 메소드 추가 필요
-        if let firstPage = tempDoc?.page(at: 0) {
-            let width = firstPage.bounds(for: .mediaBox).width
-            let height = firstPage.bounds(for: .mediaBox).height
+        if url.startAccessingSecurityScopedResource() {
             
-            let image = firstPage.thumbnail(of: .init(width: width, height: height), for: .mediaBox)
-            let thumbnailData = image.pngData()
+            let result: PDFInfo = try await NetworkManager.fetchPDFExtraction(process: .processHeaderDocument, pdfURL: url)
             
-            paperInfos.append(.init(
-                title: result.title ?? url.lastPathComponent,
-                datetime: result.date?.date ?? "알 수 없음",
-                author: names,
-                year: result.date?.date ?? "알 수 없음",
-                pages: pageCount ?? 0,
-                publisher: "알 수 없음",
-                thumbnail: thumbnailData!,
-                url: url.absoluteString)
-            )
+            let names = result.names?.reduce("") { $0 + $1 + "," } ?? "알 수 없음"
+            
+            let tempDoc = PDFDocument(url: url)
+            
+            let pageCount = tempDoc?.pageCount
+            
+            //TODO: 데이터 저장 메소드 추가 필요
+            if let firstPage = tempDoc?.page(at: 0) {
+                let width = firstPage.bounds(for: .mediaBox).width
+                let height = firstPage.bounds(for: .mediaBox).height
+                
+                let image = firstPage.thumbnail(of: .init(width: width, height: height), for: .mediaBox)
+                let thumbnailData = image.pngData()
+                
+                paperInfos.append(.init(
+                    title: result.title ?? url.lastPathComponent,
+                    datetime: result.date?.date ?? "알 수 없음",
+                    author: names,
+                    year: result.date?.date ?? "알 수 없음",
+                    pages: pageCount ?? 0,
+                    publisher: "알 수 없음",
+                    thumbnail: thumbnailData!,
+                    url: url.absoluteString)
+                )
+            } else {
+                paperInfos.append(.init(
+                    title: result.title ?? url.lastPathComponent,
+                    datetime: result.date?.date ?? "알 수 없음",
+                    author: names,
+                    year: result.date?.date ?? "알 수 없음",
+                    pages: pageCount ?? 0,
+                    publisher: "알 수 없음",
+                    thumbnail: .init(),
+                    url: url.absoluteString)
+                )
+            }
+            
+
+            print(result)
+            url.stopAccessingSecurityScopedResource()
         } else {
-            paperInfos.append(.init(
-                title: result.title ?? url.lastPathComponent,
-                datetime: result.date?.date ?? "알 수 없음",
-                author: names,
-                year: result.date?.date ?? "알 수 없음",
-                pages: pageCount ?? 0,
-                publisher: "알 수 없음",
-                thumbnail: .init(),
-                url: url.absoluteString)
-            )
+            throw NetworkManagerError.invalidURL
         }
-        
-        print(result)
     }
 }
 
