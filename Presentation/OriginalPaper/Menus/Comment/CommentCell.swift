a8172b6d1d915ad7d05b54bf184766925d362d7d
Dorii0513 <bobodorii0513@gmail.com>
2024-11-20 13:23:57 +0900

    [#232] 수정 / 삭제 뷰 디자인

--- Presentation/OriginalPaper/Menus/Comment/CommentCell.swift
+++ Presentation/OriginalPaper/Menus/Comment/CommentCell.swift
@@ -38,34 +38,39 @@ struct CommentCell: View {
             Menu {
                 ControlGroup {
                     Button(action: {
+                        //수정 액션
                         viewModel.comment = comment
                         viewModel.isEditMode = true
                         pdfViewModel.isCommentTapped = false
                     }, label: {
-                        HStack{
+                        VStack(alignment: .center, spacing: 3) {
                             Image(systemName: "pencil.line")
-                                .font(.system(size: 12))
-                                .padding(.trailing, 6)
+                                .font(.system(size: 14))
+                            
                             Text("수정")
-                                .reazyFont(.body3)
+                                .reazyFont(.h3)
                         }
                     })
                     .foregroundStyle(.gray600)
                     
                     Button(action: {
+                        // 삭제 액션
                         viewModel.deleteComment(commentId: comment.id)
                         pdfViewModel.isCommentTapped = false
                         pdfViewModel.setHighlight(selectedComments: pdfViewModel.selectedComments, isTapped: pdfViewModel.isCommentTapped)
                     }, label: {
-                        HStack{
+                        VStack(alignment: .center, spacing: 3) {
                             Image(systemName: "trash")
-                                .font(.system(size: 12))
-                                .padding(.trailing, 6)
+                                .font(.system(size: 14))
+                            
                             Text("삭제")
-                                .reazyFont(.body3)
+                                .reazyFont(.h3)
                         }
-                    }).foregroundStyle(.gray600)
+                    })
+                    .foregroundStyle(.gray600)
                 }
+                .foregroundStyle(.gray100)
+                .border(.primary2, width: 1)
             } label: {
                 Image(systemName: "ellipsis.circle")
                     .foregroundStyle(.gray500)
@@ -75,3 +80,59 @@ struct CommentCell: View {
         .padding([.trailing, .bottom], 12)
     }
 }
+
+// 수정,삭제 뷰
+struct CommentMenuView: View {
+    @EnvironmentObject var pdfViewModel: MainPDFViewModel
+    @StateObject var viewModel: CommentViewModel
+    var comment: Comment // 선택된 comment
+    
+    var body: some View {
+        HStack{
+                Button(action: {
+                    //수정 액션
+                    viewModel.comment = comment
+                    viewModel.isEditMode = true
+                    pdfViewModel.isCommentTapped = false
+                }, label: {
+                    VStack(alignment: .center, spacing: 3) {
+                        Image(systemName: "pencil.line")
+                            .font(.system(size: 14))
+                        
+                        Text("수정")
+                            .reazyFont(.h3)
+                    }
+                })
+                .foregroundStyle(.gray600)
+            
+            Divider().frame(width: 1, height: 43)
+                .tint(.primary2)
+                .padding(.horizontal, 33)
+                
+                Button(action: {
+                    viewModel.deleteComment(commentId: comment.id)
+                    pdfViewModel.isCommentTapped = false
+                    pdfViewModel.setHighlight(selectedComments: pdfViewModel.selectedComments, isTapped: pdfViewModel.isCommentTapped)
+                }, label: {
+                    VStack(alignment: .center, spacing: 3) {
+                        Image(systemName: "trash")
+                            .font(.system(size: 14))
+                        
+                        Text("삭제")
+                            .reazyFont(.h3)
+                    }
+                })
+                .foregroundStyle(.gray600)
+            }
+            .padding(.horizontal, 32)
+            .padding(.vertical, 15)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.gray100)
+                    .border(.primary2, width: 1)
+            )
+            .frame(minWidth: 130)
+            .shadow(color: Color(hex: "#6E6E6E").opacity(0.25), radius: 10, x: 0, y: 2)
+        }
+    }
+
