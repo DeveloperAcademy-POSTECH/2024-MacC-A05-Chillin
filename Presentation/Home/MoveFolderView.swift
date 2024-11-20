ef5ff86474f65352e3df8d862e528f2edfa2267e
wltnryu <yebbrnjs@gmail.com>
2024-11-21 00:39:59 +0900

    [#235] 폴더 이동 UI 구현

---
+++ Presentation/Home/MoveFolderView.swift
@@ -0,0 +1,18 @@
+//
+//  MoveFolderView.swift
+//  Reazy
+//
+//  Created by 유지수 on 11/20/24.
+//
+
+import SwiftUI
+
+struct MoveFolderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoveFolderView()
+}
