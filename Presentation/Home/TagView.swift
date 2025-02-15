68c65cf4cdb3ade2ffd67f0b6ed16ad51610d3c5
Dorii0513 <bobodorii0513@gmail.com>
2025-02-15 22:52:52 +0900

    [#465] tagEmpty 상태의 뷰 그리기

--- Presentation/Home/TagView.swift
+++ Presentation/Home/TagView.swift
@@ -9,39 +9,112 @@ import SwiftUI
 
 // MARK: - [쿠로] 태그 뷰!
 struct TagView: View {
+    
+    @State var isTagSelected: Bool = false
+    @State var isTagExist: Bool = false
+    
+    let tags: [Tag] = [
+        Tag(id: UUID(), title: "tag1", isSelected: false),
+        Tag(id: UUID(), title: "tag2", isSelected: false)
+    ]
+    
     var body: some View {
         ZStack(alignment: .top) {
             Color.gray300
-            VStack {
-                HStack(spacing: 0) {
-                    Text("태그로 원하는 논문을 찾아보세요")
-                        .foregroundStyle(.gray550)
-                    Spacer()
-                    Button(action: {
+            
+            GeometryReader { geometry in
+                VStack(spacing: 1) {
+                    
+                    // 선택된 태그 화면
+                    Group {
+                        if isTagSelected {
+                            SelectedTagView()
+                        } else {
+                            TagLEmptyView()
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 15)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(.gray100)
+                            .stroke(Color.gray400, lineWidth: 1)
+                    )
+                    
+                    // 태그 선택하는 화면
+                    VStack {
+                        Group {
+                            if isTagExist {
+                                TagListView()
+                            } else {
+                                Text("아직 태그를 만들지 않았어요")
+                                    .reazyFont(.text1)
+                                    .foregroundStyle(.gray550)
+                            }
+                        }
+                        .frame(minHeight: geometry.size.height * 0.35)
                         
-                    }, label: {
-                        Image(systemName: "chevron.down")
-                            .font(.system(size: 16))
-                            .foregroundStyle(.gray600)
-                    })
+                        // 편집 버튼
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                
+                            }, label: {
+                                Image(systemName: "ellipsis.circle")
+                                    .foregroundStyle(.gray)
+                            })
+                            .padding([.trailing, .bottom], 20)
+                        }
+                    }
+                    .frame(maxWidth: .infinity,maxHeight: geometry.size.height * 0.4)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(.gray200)
+                    )
                 }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 15)
-                .background(.gray100)
-                .clipShape(
-                    RoundedRectangle(cornerRadius: 12)
-                )
+                .padding([.top, .horizontal], 20)
             }
-            .padding([.top,.horizontal], 20)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .ignoresSafeArea(.all)
     }
 }
 
+struct TagLEmptyView: View {
+    var body: some View {
+        HStack(spacing: 0) {
+            Text("태그로 원하는 논문을 찾아보세요")
+                .reazyFont(.button1)
+                .foregroundStyle(.gray550)
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "chevron.down")
+                    .font(.system(size: 16))
+                    .foregroundStyle(.gray600)
+            })
+        }
+    }
+}
+
+struct SelectedTagView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                Text("hi")
+            }
+        }
+    }
+}
+
 struct TagListView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView(.vertical){
+            VStack(spacing: 0) {
+                Text("hi")
+            }
+        }
     }
 }
 
