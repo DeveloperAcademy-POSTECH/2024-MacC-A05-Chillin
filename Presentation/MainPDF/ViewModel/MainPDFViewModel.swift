f40e65d33608156da65b869bdfeda99a2fc94605
mooninbeom <dlsqja567@naver.com>
2025-03-09 18:13:49 +0900

    [#483] 오류 수정

--- Presentation/MainPDF/ViewModel/MainPDFViewModel.swift
+++ Presentation/MainPDF/ViewModel/MainPDFViewModel.swift
@@ -126,12 +126,12 @@ final class MainPDFViewModel: ObservableObject {
 
 // MARK: - 초기 세팅 메소드
 extension MainPDFViewModel {
-    public func savePDF(pdfView: PDFView) {
-        print("savePDF")
+    public func savePDF(pdfView: PDFView) throws {
+        var a = false
         guard let document = pdfView.document else { return }
-        guard let pdfURL = document.documentURL else {
+        guard let pdfURL = PDFSharedData.shared.paperInfo?.url, let url = try? URL(resolvingBookmarkData: pdfURL, bookmarkDataIsStale: &a) else {
             print("PDF URL을 찾을 수 없습니다.")
-            return
+            throw HomeViewError.cannotCreateBookmark
         }
         
         for pageIndex in 0..<document.pageCount {
@@ -148,7 +148,7 @@ extension MainPDFViewModel {
         // PDF 파일을 지정한 URL에 덮어쓰기 저장
         do {
             let pdfData = document.dataRepresentation()
-            try pdfData?.write(to: pdfURL)
+            try pdfData?.write(to: url)
             print("PDF 저장이 완료되었습니다.")
         } catch {
             print("PDF 저장 중 오류 발생: \(error.localizedDescription)")
