5271780f578d5e9398c659c86f3947f089e24cea
Bluecho97 <bluecho97@gmail.com>
2025-04-08 01:54:25 +0900

    [#499] PDF 공유시 하이라이트 저장

--- Presentation/MainPDF/PDFInfoMenu.swift
+++ Presentation/MainPDF/PDFInfoMenu.swift
@@ -215,7 +215,10 @@ struct PDFInfoMenu: View {
             
             // 각 페이지의 모든 주석을 반복하며 밑줄과 코멘트 아이콘 지우기
             for annotation in page.annotations {
-                if annotation.value(forAnnotationKey: .contents) != nil {
+                guard let contents = annotation.contents else { continue }
+                
+                // 하이라이트의 contents가 "UH|"로 시작하면 지우지 않기
+                if !contents.hasPrefix("UH|") {
                     page.removeAnnotation(annotation)
                 }
             }
