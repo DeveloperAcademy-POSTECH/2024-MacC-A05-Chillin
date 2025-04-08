aea80f51bde635dd4f78e304ca0105d56f53442e
유지수 / 브리(Brea)🌱 <yebbnrjs@gmail.com>
2025-04-08 19:24:11 +0900

    [#495] CustomAlert init 코드 수정

--- DesignSystem/Views/CustomAlert.swift
+++ DesignSystem/Views/CustomAlert.swift
@@ -11,12 +11,12 @@ struct CustomAlert: View {
     var type: AlertType
     
     let mainText: String
-    var message: String? = nil
+    let message: String?
     let width: CGFloat
     let height: CGFloat
     
     let cancelAction: () -> Void
-    var confirmAction: () -> Void = {}
+    let confirmAction: () -> Void
     
     init(
         type: AlertType = .delete,
@@ -25,7 +25,7 @@ struct CustomAlert: View {
         width: CGFloat,
         height: CGFloat,
         cancelAction: @escaping () -> Void,
-        confirmAction: @escaping () -> Void
+        confirmAction: @escaping () -> Void = {}
     ) {
         self.type = type
         self.mainText = mainText
