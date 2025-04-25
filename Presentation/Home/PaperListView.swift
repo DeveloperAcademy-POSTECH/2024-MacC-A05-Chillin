b3813e37e615785154ab967b35d74fb542b8ecdf
mooninbeom <dlsqja567@naver.com>
2025-04-25 13:08:27 +0900

    [#517] 태그 길어지는 현상 수정

--- Presentation/Home/PaperListView.swift
+++ Presentation/Home/PaperListView.swift
@@ -98,7 +98,7 @@ struct PaperListView: View {
                                             HomePDFCell(
                                                 paperInfo: paperInfo,
                                                 cellStatus: homeViewModel.selectedMenu == .edit ? .selection : .normal,
-                                                screenWidth: isPortrait ? geo.size.width * 0.6 :  geo.size.width * 0.73,
+                                                screenWidth: isPortrait ? geo.size.width * 0.6 :  geo.size.width * 0.7,
                                                 onTapGesture: {
                                                     navigateToPaper(paperInfo.id)
                                                     homeViewModel.updateLastModifiedDate(at: paperInfo.id, lastModifiedDate: Date())
