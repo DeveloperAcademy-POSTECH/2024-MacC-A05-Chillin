a88708be58c709dfbb7423b24d0104168fa8523a
유지수 / 브리(Brea)🌱 <yebbnrjs@gmail.com>
2025-04-27 21:35:01 +0900

    [#511] 태그 두 개 이상 선택 시 생기는 오류 해결

--- Presentation/Home/HomeSearch/Cell/PDFTagCell.swift
+++ Presentation/Home/HomeSearch/Cell/PDFTagCell.swift
@@ -30,10 +30,10 @@ struct PDFTagCell<Tag: DynamicCell>: View {
                     .reazyFont(isMultiSelectable ? .body1 : .h3)
                     .foregroundStyle(tag.isSelected ? .gray300 : .gray800)
                     .fixedSize(horizontal: true, vertical: false)
-                    .padding(.trailing, 4)
                 
                 // 삭제 버튼
                 if isEditMode {
+                    Spacer().frame(width: 4)
                     Button {
                         deleteAction()
                     } label: {
