b5a721435e0f680972e104afeaaf75443227172a
Mooninbeom <dlsqja567@naver.com>
2026-04-21 21:09:35 +0900

    [#546] 맥용 코멘트 문구 변경

--- Presentation/OriginalPaper/Menus/TemporaryAlertView.swift
+++ Presentation/OriginalPaper/Menus/TemporaryAlertView.swift
@@ -46,7 +46,7 @@ struct TemporaryAlertView: View {
         case "drawing":
             return String(localized: "하이라이트 사용 시 텍스트를 꾹 눌러 선택하세요")
         case "comment":
-            return String(localized: "코멘트를 남길 부분을 꾹 눌러 선택하세요")
+            return String(localized: ProcessInfo.processInfo.isiOSAppOnMac ? "코멘트를 남길 부분을 드래그하여 선택하세요" : "코멘트를 남길 부분을 꾹 눌러 선택하세요")
         case "translate":
             return String(localized: "번역할 텍스트를 꾹 눌러 선택하세요")
         case "lasso":
