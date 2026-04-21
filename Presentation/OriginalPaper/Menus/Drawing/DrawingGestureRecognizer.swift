d5910817d2e038d26a126605bbe3ee3862c0a2ea
Minjung Lee <minjung0067@naver.com>
2026-04-21 20:03:11 +0900

    [#547] 필기감 개선

--- Presentation/OriginalPaper/Menus/Drawing/DrawingGestureRecognizer.swift
+++ Presentation/OriginalPaper/Menus/Drawing/DrawingGestureRecognizer.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-// 터치 타입을 정의하는 열거형
 enum TouchType {
     case direct
     case indirect
@@ -20,15 +19,33 @@ protocol DrawingGestureRecognizerDelegate: AnyObject {
     func gestureRecognizerEnded(_ location: CGPoint)
 }
 
-// PDF 위에서 일어나는 제스처들을 다루는 부분
-class DrawingGestureRecognizer: UIGestureRecognizer {
+class DrawingGestureRecognizer: UIGestureRecognizer, UIGestureRecognizerDelegate {
+    override init(target: Any?, action: Selector?) {
+        super.init(target: target, action: action)
+        self.delegate = self
+    }
     weak var drawingDelegate: DrawingGestureRecognizerDelegate?
+    var isCaptureMode: Bool = false
+    
+    // 스무딩용 포인트 버퍼
+    private var pointBuffer: [CGPoint] = []
+    private let bufferSize = 3 // 값이 클수록 더 부드럽고 느리게 반응, 1~5 권장
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if ProcessInfo.processInfo.isiOSAppOnMac {
+            return isCaptureMode
+        }
+        return true
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first,
             let numberOfTouches = event?.allTouches?.count,
             numberOfTouches == 1 {
             state = .began
+            
+            // 시작 시 버퍼 초기화
+            pointBuffer.removeAll()
             
             let location = touch.location(in: self.view)
             drawingDelegate?.gestureRecognizerBegan(location)
@@ -41,7 +58,20 @@ class DrawingGestureRecognizer: UIGestureRecognizer {
         state = .changed
         
         guard let location = touches.first?.location(in: self.view) else { return }
-        drawingDelegate?.gestureRecognizerMoved(location)
+        
+        // 버퍼에 포인트 추가
+        pointBuffer.append(location)
+        if pointBuffer.count > bufferSize {
+            pointBuffer.removeFirst()
+        }
+        
+        // 버퍼 평균값으로 스무딩
+        let smoothed = CGPoint(
+            x: pointBuffer.map(\.x).reduce(0, +) / CGFloat(pointBuffer.count),
+            y: pointBuffer.map(\.y).reduce(0, +) / CGFloat(pointBuffer.count)
+        )
+        
+        drawingDelegate?.gestureRecognizerMoved(smoothed)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -49,11 +79,16 @@ class DrawingGestureRecognizer: UIGestureRecognizer {
             state = .ended
             return
         }
+        
+        // 끝날 때 버퍼 초기화
+        pointBuffer.removeAll()
+        
         drawingDelegate?.gestureRecognizerEnded(location)
         state = .ended
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+        pointBuffer.removeAll()
         state = .failed
     }
 }
